aca43b4dc24eee1dfeeb316d038d7ee7d5df3ff1
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-17 12:06:00 -0500

    Initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Form(props) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Confirm Password" />
+      {!props.userIsRegistered ? (
+        <button type="submit">Register</button>
+      ) : (
+        <button type="submit">Login</button>
+      )}
+    </form>
+  );
+}
+
+export default Form;
